22f4a0df2a98bd9132e9cf1da924eb52c7ad23a2
july-gusman <julianagusman95@gmail.com>
2024-06-20 17:13:34 -0300

    Concluindo o projeto desafio poo.

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -3,34 +3,36 @@ package br.com.dio.desafio.dominio;
 import br.com.dio.desafio.Conteudo;
 
 public class Curso extends Conteudo{
-	
-	private int cargaHoraria;
-	
-	
-	@Override
-	public double calcularXp() {
-		return XP_PADRAO = cargaHoraria;
-	}
-	
-	
+
+    private int cargaHoraria;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;}
+
     public Curso() {
+    }
+
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+
+	public void setTitulo(String string) {
+		// TODO Auto-generated method stub
 		
 	}
-	
-	
-	public int getCargaHoraria() {
-		return cargaHoraria;
-	}
-	public void setCargaHoraria(int cargaHoraria) {
-		this.cargaHoraria = cargaHoraria;
-	}
-	@Override
-	public String toString() {
-		return "Curso [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", cargaHoraria=" + cargaHoraria + "]";
-	}
-
-
-    
-	
-	
 }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -6,29 +6,30 @@ import br.com.dio.desafio.Conteudo;
 
 public class Mentoria extends Conteudo{
 
-	
-	private LocalDate data;
-	
-	@Override
-	public double calcularXp() {
-         return XP_PADRAO + 20d;
-	}
-	
-	public Mentoria() {
-		
-	}
-	
-	
-	public LocalDate getData() {
-		return data;
-	}
-	public void setData(LocalDate data) {
-		this.data = data;
-	}
-	@Override
-	public String toString() {
-		return "Mentoria [titulo=" + getTitulo() + ", descricao=" + getDescricao() + ", data=" + data + "]";
-	}
-	
-	
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public Mentoria() {
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
 }
